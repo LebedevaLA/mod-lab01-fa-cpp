10feca934c3498eb10ae84ec38f3bae516a98221
LebedevaLA <alldifferent.6489@gmail.com>
2025-03-13 00:44:13 +0300

    Load all Solution

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,127 @@
-// Copyright 2022 UNN-IASR
+#include <cstring>
+#include <cmath>
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
-}
+bool is_letter_up(char a) {
+    if ('A' <= a && a <= 'Z') {
+      return true;
+    }
+    return false;
+  }
 
+  bool is_letter_down(char a) {
+    if ('a' <= a && a <= 'z') {
+      return true;
+    }
+    return false;
+  }
+
+  bool is_number(char a) {
+    if ('0' <= a && a <= '9') {
+      return true;
+    }
+    return false;
+  }
+
+unsigned int faStr1(const char *str) {
+    if (strlen(str) == 0) {
+      return 0;
+    }
+    int counter = 0;
+    char last = str[0];
+    int i = 1;
+    bool good;
+    if (is_number(last)) {
+      good = false;
+    } else {
+      good = true;
+    }
+    while (i < strlen(str)) {
+      if (last != ' ' && str[i] == ' ') {
+        if (good) {
+          counter++;
+        }
+        good = true;
+      } else {
+        if (is_number(str[i])) {
+          good = false;
+        }
+      }
+      last = str[i];
+      i++;
+    }
+    if (good && last != ' ' && !is_number(last)) {
+      counter++;
+    }
+    return counter;
+}
 unsigned int faStr2(const char *str) {
-    return 0;
+    if (strlen(str) == 0) {
+      return 0;
+    }
+    char last = str[0];
+    if (strlen(str) == 1) {
+      if (is_letter_up(last)) {
+        return 1;
+      }
+      return 0;
+    }
+    int i = 1;
+    bool good;
+    int counter = 0;
+    if (last == ' ' || is_letter_up(last)) {
+      good = true;
+    } else {
+      good = false;
+    }
+    while (i < strlen(str)) {
+      if (str[i] == ' ') {
+        if (last != ' ' && good) {
+          counter++;
+        }
+        good = true;
+      } else {
+        if ((last == ' ' && !is_letter_up(str[i])) ||
+        (last != ' ' && !is_letter_down(str[i]))) {
+          good = false;
+        }
+      }
+      last = str[i];
+      i++;
+    }
+    if ((str[i - 2] == ' ' && is_letter_up(last)) ||
+    (str[i - 2] != ' ' && good && is_letter_down(last))) {
+      counter++;
+    }
+    return counter;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    if (strlen(str) == 0) {
+      return 0;
+    }
+    int counter = 0;
+    int letter = 0;
+    char last = str[0];
+    if (last != ' ') {
+      letter++;
+    }
+    int i = 1;
+    while (i < strlen(str)) {
+      if (str[i] != ' ') {
+        letter++;
+      }
+      if (last != ' ' && str[i] == ' ') {
+        counter++;
+      }
+      last = str[i];
+      i++;
+    }
+    if (last != ' ') {
+      counter++;
+    }
+    if (counter == 0) {
+      return 0;
+    }
+    return round(letter / counter);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
 // Copyright 2022 UNN-IASR
+#include<iostream>
 #include "fun.h"
 
 int main() {
-    return 0;
+  std :: cout << ("First func")<< std :: endl;
+  std :: cout << (" Ng Er") << faStr1(" Ng Er") << std :: endl;
+  std :: cout << ("   ") << faStr1("   ") << std :: endl;
+  std :: cout << ("") << faStr1("") << std :: endl;
+  std :: cout << ("NgEr1") << faStr1("NgEr1") << std :: endl;
+  std :: cout << ("N") << faStr1("N") << std :: endl;
+  std :: cout << (" NgEr1 rt") << faStr1(" NgEr1 rt") << std :: endl;
+  std :: cout << (" NgEr1 rt ") << faStr1(" NgEr1 rt ") << std :: endl;
+  std :: cout << (" NgEr1 rt1") << faStr1(" NgEr1 rt1") << std :: endl;
+  std :: cout << (" NgEr1 4rt") <<
+  faStr1(" NgEr1 4rt") << std :: endl << std :: endl;
+  std :: cout << ("Second func")<< std :: endl;
+  std :: cout << (" Ng Er trE") << faStr2(" Ng Er trE") << std :: endl;
+  std :: cout << ("   ") << faStr2("   ") << std :: endl;
+  std :: cout << ("") << faStr2("") << std :: endl;
+  std :: cout << ("Ng") << faStr2("Ng") << std :: endl;
+  std :: cout << ("rt") << faStr2("rt") << std :: endl;
+  std :: cout << ("N") << faStr2("N") << std :: endl;
+  std :: cout << ("b") << faStr2("b") << std :: endl;
+  std :: cout << (" Ng  E") << faStr2(" Ng  E") << std :: endl;
+  std :: cout << (" Ng  Er") << faStr2(" Ng  Er") << std :: endl;
+  std :: cout << (" Ng  E ") << faStr2(" Ng  E ") << std :: endl;
+  std :: cout << (" Ng  Er ") << faStr2(" Ng  Er ") << std :: endl;
+  std :: cout << (" Ng  E") << faStr2(" Ng  E") << std :: endl;
+  std :: cout << (" Ng  r") << faStr2(" Ng  r") << std :: endl << std :: endl;
+  std :: cout << ("Third func")<< std :: endl;
+  std :: cout << ("Ng  Et") << faStr3("Ng  Et") << std :: endl;
+  std :: cout << ("") << faStr3("") << std :: endl;
+  std :: cout << ("Ng") << faStr3("Ng") << std :: endl;
+  std :: cout << ("Ng tdf") << faStr3("Ng tdf") << std :: endl;
+  std :: cout << ("Ng vb") << faStr3("Ng vb") << std :: endl;
+  std :: cout << ("Ngtd tr1fd") << faStr3("Ngtd tr1fd") << std :: endl;
+  std :: cout << ("       ") << faStr3("       ") << std :: endl;
+  return 0;
 }
